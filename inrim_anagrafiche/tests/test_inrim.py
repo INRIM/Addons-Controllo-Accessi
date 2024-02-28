7838e6475a54e293733739840a803aa923557626
Alessio Gerace <alessio.gerace@gmail.com>
2024-02-28 11:07:40 +0100

    [FIX] docstring in tests [IMP] code optimization

--- inrim_anagrafiche/tests/test_inrim.py
+++ inrim_anagrafiche/tests/test_inrim.py
@@ -1,19 +1,25 @@
-from odoo.tests import tagged
-from odoo.addons.inrim_anagrafiche.tests.common import TestCommon
-from odoo.exceptions import AccessError, UserError
 import base64
 from datetime import date
+
 from dateutil.relativedelta import relativedelta
+from odoo.addons.inrim_anagrafiche.tests.common import TestCommon
+from odoo.exceptions import UserError
+from odoo.tests import tagged
+
 
 @tagged("post_install", "-at_install", "inrim")
 class TestInrim(TestCommon):
 
     # Test 1
-    '''
-    Descrizione:
-        Verifica che tutti i models in Dati per Test abbiano i dati
-    '''
+
     def test_1(self):
+        """
+        Descrizione:
+            Verifica che tutti i models in Dati per Test abbiano i dati
+
+        :return:  Corrispondenza dei dati per numero di record alla search
+        """
+
         self.assertTrue(self.user)
         self.assertTrue(self.user_1)
         self.assertTrue(self.user_2)
@@ -38,38 +44,42 @@ class TestInrim(TestCommon):
         self.assertTrue(self.tag_6)
         self.assertTrue(self.lettore_1)
         self.assertTrue(self.lettore_2)
-    '''
-    Risultato Atteso:
-        Corrispondenza dei dati per numero di record alla search
-    '''
 
     # Test 2
     '''
     Descrizione:
-        Utente1 crea una Sede 2
+       
     '''
+
     def test_2(self):
+        """
+         Utente1 crea una Sede 2
+        :return: Esiste Sede 2 in ente azienda
+        """
         self.env = self.env(user=self.user_1)
         self.cr = self.env.cr
         ca_ente_azienda_id = self.env['ca.ente_azienda'].create({
-            'name' : 'Sede 2',
+            'name': 'Sede 2',
             'pec': 'Pec',
-            'tipo_ente_azienda_id': self.env.ref('inrim_anagrafiche.tipo_ente_azienda_sede').id,
+            'tipo_ente_azienda_id': self.env.ref(
+                'inrim_anagrafiche.tipo_ente_azienda_sede').id,
             'note': 'Note'
         })
         self.assertTrue(ca_ente_azienda_id)
-    '''
-    Risultato Atteso:
-        Esiste Sede 2 in ente azienda
-    '''
 
     # Test 3
     '''
     Descrizione:
-        Utente1 crea un record Persona 6 di tipo: 
-        esterno, servizi, collegato ad Azienda Test con doc identita’
+        
     '''
+
     def test_3(self):
+        """
+        Utente1 crea un record Persona 6 di tipo:
+        esterno, servizi, collegato ad Azienda Test con doc identita’
+
+        :return: Esiste il record Persona 6 stato Completata
+        """
         self.env = self.env(user=self.user_1)
         self.cr = self.env.cr
         ca_persona_id = self.env['ca.persona'].create({
@@ -77,14 +87,17 @@ class TestInrim(TestCommon):
             'lastname': '6',
             'fiscalcode': 'Fiscalcode1',
             'type_ids': [(6, 0,
-                [
-                    self.env.ref('inrim_anagrafiche.tipo_persona_esterno').id,
-                    self.env.ref('inrim_anagrafiche.tipo_persona_servizi').id
-                ]
-            )],
+                          [
+                              self.env.ref(
+                                  'inrim_anagrafiche.tipo_persona_esterno').id,
+                              self.env.ref(
+                                  'inrim_anagrafiche.tipo_persona_servizi').id
+                          ]
+                          )],
             'ca_ente_azienda_ids': [(6, 0, [self.ente_azienda_3.id])],
             'ca_documento_ids': self.env['ca.documento'].create({
-                'tipo_documento_id': self.env.ref('inrim_anagrafiche.tipo_doc_ident_carta_identita').id,
+                'tipo_documento_id': self.env.ref(
+                    'inrim_anagrafiche.tipo_doc_ident_carta_identita').id,
                 'validity_start_date': date(2024, 1, 1),
                 'validity_end_date': date(2024, 3, 31),
                 'image_ids': [(6, 0, [
@@ -105,55 +118,54 @@ class TestInrim(TestCommon):
             })
         })
         self.assertTrue(ca_persona_id)
-    '''
-    Risultato Atteso:
-        Esiste il record Persona 6 stato Completata
-    '''
 
     # Test 4
     '''
     Descrizione:
-        Utente3 modifica Persona 6
-    '''
-    def test_4(self):
-        self.assertTrue(self.env['ca.persona'].with_user(self.user_3).check_access_rights('write'))
-    '''
-    Risultato Atteso:
-        Utente3 modifica con successo persona 6
+        
     '''
 
+    def test_4(self):
+        """
+         Utente3 modifica Persona 6
+
+        :return: Utente3 modifica con successo persona 6
+        """
+        self.assertTrue(
+            self.env['ca.persona'].with_user(self.user_3).check_access_rights(
+                'write'))
+
     # Test 5
-    '''
-    Descrizione:
-        Tag 2 in Tag → “in Uso” e’ False
-    '''
     def test_5(self):
+        """
+        Tag 2 in Tag → “in Uso” e’ False
+        :return: Vero
+        """
         self.assertFalse(self.tag_2.in_use)
-    '''
-    Risultato Atteso:
-        Vero
-    '''
 
     # Test 6
     '''
     Descrizione:
+
+    '''
+
+    def test_6(self):
+        """
         Utente1 crea un record Tag Persona collegando Persona
         6 a Tag 2 , inizio oggi fine oggi + 3gg
-    '''
-    def test_6(self):
+
+        :return: Esiste il nuovo Record Tag 2 in Tag → “in Uso” True
+        """
         self.env = self.env(user=self.user_1)
         self.cr = self.env.cr
         ca_tag_persona_id = self.env['ca.tag_persona'].create({
             'ca_persona_id': self.persona_6.id,
             'ca_tag_id': self.tag_2.id,
             'date_start': date.today(),
-            'date_end': date.today() + relativedelta(days = 3)
+            'date_end': date.today() + relativedelta(days=3)
         })
-        self.assertTrue(ca_tag_persona_id and ca_tag_persona_id.ca_tag_id.in_use)
-    '''
-    Risultato Atteso:
-        Esiste il nuovo Record Tag 2 in Tag → “in Uso” True
-    '''
+        self.assertTrue(
+            ca_tag_persona_id and ca_tag_persona_id.ca_tag_id.in_use)
 
     # Test 7
     '''
@@ -161,34 +173,38 @@ class TestInrim(TestCommon):
         Utente1 crea un record Tag Persona collegando Persona
         6 a Tag 2 , inizio oggi fine oggi +2
     '''
+
     def test_7(self):
+        """
+        Utente1 crea un record Tag Persona collegando Persona
+        6 a Tag 2 , inizio oggi fine oggi +2
+
+        :return: Errore:” Tag 2 gia in uso nel periodo”
+        """
         self.env = self.env(user=self.user_1)
         self.cr = self.env.cr
         self.env['ca.tag_persona'].create({
             'ca_persona_id': self.persona_6.id,
             'ca_tag_id': self.tag_2.id,
             'date_start': date.today(),
-            'date_end': date.today() + relativedelta(days = 3)
+            'date_end': date.today() + relativedelta(days=3)
         })
         with self.assertRaises(UserError):
             self.env['ca.tag_persona'].create({
                 'ca_persona_id': self.persona_6.id,
                 'ca_tag_id': self.tag_2.id,
                 'date_start': date.today(),
-                'date_end': date.today() + relativedelta(days = 2)
+                'date_end': date.today() + relativedelta(days=2)
             })
-    '''
-    Risultato Atteso:
-        Errore:” Tag 2 gia in uso nel periodo”
-    '''
 
     # Test 8
-    '''
-    Descrizione:
+    def test_8(self):
+        """
         Utente1 crea un record Tag Persona collegando Persona
         6 a Tag 6 , inizio oggi fine oggi +2
-    '''
-    def test_8(self):
+
+        :return: Errore: "Tag 6 risulta revocato"
+        """
         self.env = self.env(user=self.user_1)
         self.cr = self.env.cr
         with self.assertRaises(UserError):
@@ -196,20 +212,23 @@ class TestInrim(TestCommon):
                 'ca_persona_id': self.persona_6.id,
                 'ca_tag_id': self.tag_6.id,
                 'date_start': date.today(),
-                'date_end': date.today() + relativedelta(days = 2)
+                'date_end': date.today() + relativedelta(days=2)
             })
-    '''
-    Risultato Atteso:
-        Errore: "Tag 6 risulta revocato"
-    '''
 
     # Test 9
     '''
     Descrizione:
+        
+    '''
+
+    def test_9(self):
+        """
         Utente1 crea un record Tag Persona collegando Persona
         6 a Tag 3 , inizio oggi fine oggi +2
-    '''
-    def test_9(self):
+
+        :return: Errore: "Tag 3; ad un esterno possono essere
+        assegnati solo Tag di Tipo Temporaneo"
+        """
         self.env = self.env(user=self.user_1)
         self.cr = self.env.cr
         with self.assertRaises(UserError):
@@ -217,10 +236,5 @@ class TestInrim(TestCommon):
                 'ca_persona_id': self.persona_6.id,
                 'ca_tag_id': self.tag_3.id,
                 'date_start': date.today(),
-                'date_end': date.today() + relativedelta(days = 2)
+                'date_end': date.today() + relativedelta(days=2)
             })
-    '''
-    Risultato Atteso:
-        Errore: "Tag 3; ad un esterno possono essere 
-        assegnati solo Tag di Tipo Temporaneo"
-    '''
