246ca4b6be51fb691e43007bb70fecc08cc303c2
Marcoapu <m.cazzato@apuliasoftware.it>
2024-04-08 16:09:58 +0200

    [UPD] added categoria attivita masterdata

--- inrim_anagrafiche/models/ca_categoria_tipo_richiesta.py
+++ inrim_anagrafiche/models/ca_categoria_tipo_richiesta.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class CaCategoriaTipoRichiesta(models.Model):
     _name = 'ca.categoria_tipo_richiesta'
@@ -9,4 +9,15 @@ class CaCategoriaTipoRichiesta(models.Model):
     date_start = fields.Date()
     date_end = fields.Date()
     ca_categoria_richiesta = fields.Many2one('ca.categoria_richiesta')
+    is_activity = fields.Boolean(compute="_compute_is_activity", store=True)
     active = fields.Boolean(default=True)
+
+    @api.depends('ca_categoria_richiesta')
+    def _compute_is_activity(self):
+        for record in self:
+            record.is_activity = False
+            if (
+                record.ca_categoria_richiesta == self.env.ref(
+                    'inrim_anagrafiche.ca_categoria_richiesta_attivita')
+            ):
+                record.is_activity = True
--- inrim_anagrafiche/models/ca_registro_accesso.py
+++ inrim_anagrafiche/models/ca_registro_accesso.py
@@ -6,5 +6,6 @@ class CaRegistroAccesso(models.Model):
     _rec_name = 'spazio_id'
 
     spazio_id = fields.Many2one('ca.spazio', required=True)
+    ente_azienda_id = fields.Many2one(related='spazio_id.ente_azienda_id', store=True)
     righe_accesso_ids = fields.One2many('ca.righe_accesso', 'registro_accesso_id')
     active = fields.Boolean(default=True)
