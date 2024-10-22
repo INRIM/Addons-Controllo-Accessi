35f0e475979fcf7d05626e31024ceeff2d0d6eb3
Marcoapu <m.cazzato@apuliasoftware.it>
2024-10-22 17:06:16 +0200

    [ADD] added tag_lettore get,post,put rest api

--- inrim_controllo_accessi_api/controllers/lettore.py
+++ inrim_controllo_accessi_api/controllers/lettore.py
@@ -2,15 +2,12 @@ from odoo import http
 
 from .api_controller_inrim import InrimApiController, BadRequest
 
-model = "ca.lettore"
-
-
 class InrimApiLettore(InrimApiController):
 
     @http.route('/api/lettore', auth="none", type='http', methods=['GET'],
                 csrf=False)
     def api_get_ca_lettore(self, **params):
-        self.check_token(model, 'read')
+        self.check_token('ca.lettore', 'read')
 
         return self.handle_response(
             *self.model.rest_get(params), is_list=True)
@@ -18,33 +15,59 @@ class InrimApiLettore(InrimApiController):
     @http.route('/api/lettore', auth="none", type='http', methods=['POST'],
                 csrf=False)
     def api_post_ca_lettore(self):
-        self.check_token(model, 'create')
+        self.check_token('ca.lettore', 'create')
         data = self.check_and_decode_body()
         try:
             return self.handle_response(*self.model.rest_post(data))
         except Exception as e:
-            self.env.cr.rollback()
             raise BadRequest(str(e))
 
     @http.route('/api/lettore', auth="none", type='http', methods=['PUT'],
                 csrf=False)
     def api_put_ca_lettore(self):
-        self.check_token(model, 'write')
+        self.check_token('ca.lettore', 'write')
         data = self.check_and_decode_body()
         try:
             return self.handle_response(*self.model.rest_put(data))
         except Exception as e:
-            self.env.cr.rollback()
             raise BadRequest(str(e))
 
     @http.route('/api/lettore', auth="none", type='http', methods=['DELETE'],
                 csrf=False)
     def api_delete_ca_lettore(self):
-        self.check_token(model, 'unlink')
+        self.check_token('ca.lettore', 'unlink')
         data = self.check_and_decode_body()
         try:
             return self.handle_response(
                 *self.model.rest_delete(data), delete=True)
         except Exception as e:
-            self.env.cr.rollback()
+            raise BadRequest(str(e))
+        
+class InrimApiTagLettore(InrimApiController):
+
+    @http.route('/api/tag_lettore', auth="none", type='http', methods=['GET'],
+                csrf=False)
+    def api_get_ca_tag_lettore(self, **params):
+        self.check_token('ca.tag_lettore', 'read')
+        return self.handle_response(
+            *self.model.rest_get(params), is_list=True)
+
+    @http.route('/api/tag_lettore', auth="none", type='http', methods=['POST'],
+                csrf=False)
+    def api_post_ca_tag_lettore(self):
+        self.check_token('ca.tag_lettore', 'create')
+        data = self.check_and_decode_body()
+        try:
+            return self.handle_response(*self.model.rest_post(data))
+        except Exception as e:
+            raise BadRequest(str(e))
+
+    @http.route('/api/tag_lettore', auth="none", type='http', methods=['PUT'],
+                csrf=False)
+    def api_put_ca_tag_lettore(self):
+        self.check_token('ca.tag_lettore', 'write')
+        data = self.check_and_decode_body()
+        try:
+            return self.handle_response(*self.model.rest_put(data))
+        except Exception as e:
             raise BadRequest(str(e))
