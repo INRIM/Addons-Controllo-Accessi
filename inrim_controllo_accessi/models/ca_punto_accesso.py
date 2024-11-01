0e6ecd2a103fbcfff8aaf7f7deb2612b5b82713b
Alessio Gerace <alessio.gerace@gmail.com>
2024-11-01 22:09:16 +0100

    [FIX] check  access regster

--- inrim_controllo_accessi/models/ca_punto_accesso.py
+++ inrim_controllo_accessi/models/ca_punto_accesso.py
@@ -73,7 +73,7 @@ class CaPuntoAccesso(models.Model):
                 fields.datetime.now().strftime('%Y-%m-%d 23:59:59')),
             ('ca_lettore_id', '=', self.ca_lettore_id.id),
             ('access_allowed', '=', False)
-        ], order="person_lastname, datetime_event asc")
+        ], order="person_display_name, datetime_event asc")
         return {
             'name': _('Accessi Rifiutati Oggi'),
             'type': 'ir.actions.act_window',
