4f952e11ede8f15b7e84080213ae37ca445a7808
Alessio Gerace <alessio.gerace@gmail.com>
2025-03-08 09:27:33 +0100

    [FIX] portal controller

--- controllo_accessi_portale/controllers/main.py
+++ controllo_accessi_portale/controllers/main.py
@@ -1,7 +1,6 @@
 from odoo import http
 from odoo.http import request
-from odoo.addons.web.controllers.main import Home
-
+from odoo.addons.web.controllers.home import Home
 
 class CustomHome(Home):
     @http.route()
