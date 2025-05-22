0d304762e8138592b510c1f783db56a6c501c0be
Alessio Gerace <alessio.gerace@gmail.com>
2025-05-22 11:19:18 +0200

    [IMP] rest put persona

--- inrim_controllo_accessi_api/controllers/persona.py
+++ inrim_controllo_accessi_api/controllers/persona.py
@@ -2,10 +2,11 @@ from odoo import http
 
 from .api_controller_inrim import InrimApiController, BadRequest
 
+
 class InrimApiPersona(InrimApiController):
 
     @http.route('/api/persona', auth="none", type='http', methods=['GET'],
-           csrf=False)
+                csrf=False)
     def api_get_ca_persona(self, **params):
         model = 'ca.persona'
         self.check_token(model, 'read')
@@ -21,11 +22,22 @@ class InrimApiPersona(InrimApiController):
             return self.handle_response(*self.model.rest_post(data))
         except Exception as e:
             raise BadRequest(str(e))
-    
+
+    @http.route(
+        '/api/persona', auth="none", type='http', methods=['PUT'], csrf=False)
+    def api_put_ca_persona(self):
+        self.check_token('ca.persona', 'write')
+        data = self.check_and_decode_body()
+        try:
+            return self.handle_response(*self.model.rest_put(data))
+        except Exception as e:
+            raise BadRequest(str(e))
+
+
 class InrimApiTipoPersona(InrimApiController):
 
     @http.route('/api/tipo_persona', auth="none", type='http', methods=['GET'],
-           csrf=False)
+                csrf=False)
     def api_get_ca_tipo_persona(self, **params):
         model = 'ca.tipo_persona'
         self.check_token(model, 'read')
