c8b31ec113049604f599c13cc0a702c069a213d4
Alessio Gerace <alessio.gerace@gmail.com>
2025-12-29 16:39:02 +0100

    [FIX] check reader person

--- inrim_controllo_accessi/models/ca_lettore_persona.py
+++ inrim_controllo_accessi/models/ca_lettore_persona.py
@@ -120,7 +120,7 @@ class CaLettorePersona(models.Model):
 
     def check_update_by_date_valididty(self):
         for person_reader in self.env['ca.lettore_persona'].search(
-                [('date_end', '>=', fields.Datetime.now())]):
+                [('state', 'not in', ['expired'])]):
             if person_reader:
                 person_reader.check_update_state()
 
